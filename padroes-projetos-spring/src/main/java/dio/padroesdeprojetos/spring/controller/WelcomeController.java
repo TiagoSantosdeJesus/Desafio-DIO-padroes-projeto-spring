60650c9bf564c6d2bf06868d34898ef4cc9dd792
TiagoSantosdeJesus <tiagosantos2561@hotmail.com>
2023-02-27 15:29:03 -0300

    Padrões de Projetos com Spring Framework

---
+++ padroes-projetos-spring/src/main/java/dio/padroesdeprojetos/spring/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package dio.padroesdeprojetos.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to my Spring Framework Web API";
+    }
+}
